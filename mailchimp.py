29106f046548b8d76095f596204d1f8b69d1c108
Danial <bagheri.danial@gmail.com>
2018-03-05 14:23:15 +0000

    First commit

---
+++ mailchimp.py
@@ -0,0 +1,35 @@
+
+import requests
+import json
+
+"""
+This is a simple code to subscibe certain mailing list on Mailchimp. 
+You can easilt adopt it to Django by changing the subscriber section
+
+Written by: Danial Bagheri 
+github.com/danialbagheri
+"""
+subscriber = raw_input("what's your email ")
+# subscriber = "bagheri.danial@gmail.com"
+# authorisation
+api_key = '' # Your API key goes Here
+list_id = '' # Your list ID goes Here
+subdomain = '' # the letters after the dash in your api key. This is different for different account like US14
+rooturl = "https://{}.api.mailchimp.com/3.0/".format(subdomain)
+listurl = "lists/{}/members".format(list_id)
+
+
+payload = {"type" : "string",
+"email_address" : subscriber,
+"status": "subscribed",
+}
+
+mail = requests.post(rooturl+listurl, auth=('restora', api_key), data=json.dumps(payload))
+
+# This checked if the subscriber is already a member of any list or if there is any error, you can render the result to any html file too.
+if mail.json()["status"] == 400:
+	print mail.json()["title"]
+else:
+	print"You have been subscribed"
+
+
